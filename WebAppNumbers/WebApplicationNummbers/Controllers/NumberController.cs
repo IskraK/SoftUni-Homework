3f2ba3e6d7495ee0bb2ff077451f36563fc768d3
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2021-04-17 18:48:56 +0300

    Change folders

---
+++ WebAppNumbers/WebApplicationNummbers/Controllers/NumberController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationNummbers.Controllers
+{
+    public class NumberController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Generator(int id)
+        {
+            return View(id);
+        }
+
+        [HttpPost]
+        public IActionResult NumGenerator(int num)
+        {
+            //return View("Generator",num);
+            return View(nameof(Generator), num);
+        }
+    }
+}
