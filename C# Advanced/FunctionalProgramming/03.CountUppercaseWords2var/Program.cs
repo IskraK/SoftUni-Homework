7f8e792de5f25644580d745aa2828d5074865dd5
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2021-06-06 13:07:29 +0300

    Add files

---
+++ C# Advanced/FunctionalProgramming/03.CountUppercaseWords2var/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CountUppercaseWords2var
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Predicate<string> isFirstLetterCapital = str => str[0] == str.ToUpper()[0];
+
+            Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => isFirstLetterCapital(x))
+                .ToList()
+                .ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
