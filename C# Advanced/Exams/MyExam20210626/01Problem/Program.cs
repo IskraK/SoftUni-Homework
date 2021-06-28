3a643a08188a53f49aaa242ad46754f6aae3bd10
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2021-06-28 17:52:11 +0300

    Add my exam files

---
+++ C# Advanced/Exams/MyExam20210626/01Problem/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int DippingSauce = 150;
+            const int GreenSalad = 250;
+            const int ChocolateCake = 300;
+            const int Lobster = 400;
+
+            int[] ingredientsInput = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[] freshnessInput = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Queue<int> ingredients = new Queue<int>(ingredientsInput);
+            Stack<int> freashness = new Stack<int>(freshnessInput);
+
+            SortedDictionary<string, int> dishes = new SortedDictionary<string, int>();
+            dishes.Add("Dipping sauce", 0);
+            dishes.Add("Green salad", 0);
+            dishes.Add("Chocolate cake", 0);
+            dishes.Add("Lobster", 0);
+
+            while (ingredients.Count > 0 && freashness.Count > 0)
+            {
+                if (ingredients.Peek() == 0)
+                {
+                    ingredients.Dequeue();
+                    continue;
+                }
+
+                if (ingredients.Peek() * freashness.Peek() == DippingSauce)
+                {
+                    dishes["Dipping sauce"]++;
+                    ingredients.Dequeue();
+                    freashness.Pop();
+                }
+                else if (ingredients.Peek() * freashness.Peek() == GreenSalad)
+                {
+                    dishes["Green salad"]++;
+                    ingredients.Dequeue();
+                    freashness.Pop();
+                }
+                else if (ingredients.Peek() * freashness.Peek() == ChocolateCake)
+                {
+                    dishes["Chocolate cake"]++;
+                    ingredients.Dequeue();
+                    freashness.Pop();
+                }
+                else if (ingredients.Peek() * freashness.Peek() == Lobster)
+                {
+                    dishes["Lobster"]++;
+                    ingredients.Dequeue();
+                    freashness.Pop();
+                }
+                else
+                {
+                    freashness.Pop();
+                    ingredients.Enqueue(ingredients.Dequeue() + 5);
+                }
+            }
+
+            if (dishes["Dipping sauce"] >=1 && dishes["Green salad"] >= 1 && dishes["Chocolate cake"] >= 1 && dishes["Lobster"] >= 1 )
+            {
+                Console.WriteLine("Applause! The judges are fascinated by your dishes!");
+            }
+            else
+            {
+                Console.WriteLine("You were voted off. Better luck next year.");
+            }
+
+            if (ingredients.Count > 0)
+            {
+                Console.WriteLine($"Ingredients left: {ingredients.Sum()}");
+            }
+
+            var sortedDishes = dishes.Where(n => n.Value >= 1).ToDictionary(x => x.Key, y => y.Value);
+            foreach (var dish in sortedDishes)
+            {
+                Console.WriteLine($" # {dish.Key} --> {dish.Value}");
+            }
+        }
+    }
+}
---
+++ C# Advanced/Exams/MyExam20210626/02Problem/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Linq;
+
+namespace _02Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            char[][] jagged = new char[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                char[] currRow = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+                jagged[row] = new char[currRow.Length];
+
+                for (int col = 0; col < currRow.Length; col++)
+                {
+                    jagged[row][col] = currRow[col];
+
+                }
+            }
+
+            int countOfCollected = 0;
+            int countOfOpponentsTokens = 0;
+
+            string cmdLine = Console.ReadLine();
+
+            while (cmdLine != "Gong")
+            {
+                string[] cmdArgs = cmdLine.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string command = cmdArgs[0];
+                int currRow = int.Parse(cmdArgs[1]);
+                int currCol = int.Parse(cmdArgs[2]);
+
+                if (command == "Find")
+                {
+                    if (IsValidCoordinates(rows, jagged, currRow, currCol))
+                    {
+                        countOfCollected = CountTokens(jagged, countOfCollected, currRow, currCol);
+                    }
+                }
+                else if (command == "Opponent")
+                {
+                    string direction = cmdArgs[3];
+                    if (IsValidCoordinates(rows, jagged, currRow, currCol))
+                    {
+                        countOfOpponentsTokens = CountTokens(jagged, countOfOpponentsTokens, currRow, currCol);
+
+                        for (int i = 0; i < 3; i++)
+                        {
+                            currRow = MoveRow(currRow, direction);
+                            currCol = MoveCol(currCol, direction);
+
+                            if (IsValidCoordinates(rows, jagged, currRow, currCol))
+                            {
+                                countOfOpponentsTokens = CountTokens(jagged, countOfOpponentsTokens, currRow, currCol);
+                            }
+                        }
+
+                    }
+                }
+
+                cmdLine = Console.ReadLine();
+            }
+
+            for (int row = 0; row < jagged.GetLength(0); row++)
+            {
+                for (int col = 0; col < jagged[row].Length; col++)
+                {
+                    Console.Write(jagged[row][col] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"Collected tokens: {countOfCollected}");
+            Console.WriteLine($"Opponent's tokens: {countOfOpponentsTokens}");
+        }
+
+        private static int CountTokens(char[][] jagged, int countTokens, int row, int col)
+        {
+            if (jagged[row][col] == 'T')
+            {
+                countTokens++;
+                jagged[row][col] = '-';
+            }
+
+            return countTokens;
+        }
+
+        public static int MoveCol(int col, string movement)
+        {
+            if (movement == "left")
+            {
+                return col - 1;
+            }
+            if (movement == "right")
+            {
+                return col + 1;
+            }
+
+            return col;
+        }
+        public static int MoveRow(int row, string movement)
+        {
+            if (movement == "up")
+            {
+                return row - 1;
+            }
+            if (movement == "down")
+            {
+                return row + 1;
+            }
+
+            return row;
+        }
+
+        private static bool IsValidCoordinates(int rows, char[][] jagged, int row, int col)
+        {
+            return row >= 0 && row < rows && col >= 0 && col < jagged[row].Length;
+        }
+    }
+}
