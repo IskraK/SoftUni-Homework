ce88d0394f880ea35f8ac0df18e689c5082506dc
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2021-07-06 15:28:04 +0300

    Add files

---
+++ C# OOP/03.InterfacesAndAbstractionLab/Cars/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Cars
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Seat seat = new Seat("Leon", "Grey");
+            Tesla tesla = new Tesla("Model 3", "Red", 2);
+
+            Console.WriteLine(seat.Start());
+            Console.WriteLine(seat.Stop());
+
+            Console.WriteLine(tesla.Start());
+            Console.WriteLine(tesla.Stop());
+
+            Console.WriteLine(seat);
+            Console.WriteLine(tesla);
+        }
+    }
+}
---
+++ C# OOP/03.InterfacesAndAbstractionLab/Cars2var/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public abstract class Car:ICar
+    {
+        public string Model { get; private set; }
+        public string Color { get; private set; }
+
+        public Car(string model,string color)
+        {
+            Model = model;
+            Color = color;
+        }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+    }
+}
