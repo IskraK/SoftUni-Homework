7ef69d538e31ccbeb0b5cb79e55cee94addec846
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2021-07-30 21:14:31 +0300

    Add files

---
+++ C# OOP/11.DesignPatternsExercise/CompositePattern/CompositeGift.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompositePattern
+{
+    public class CompositeGift : GiftBase, IGiftOperatons
+    {
+        private ICollection<GiftBase> gifts;
+        public CompositeGift(string name, int price) 
+            : base(name, price)
+        {
+            gifts = new HashSet<GiftBase>();
+        }
+
+        public void Add(GiftBase gift)
+        {
+            gifts.Add(gift);
+        }
+        public void Remove(GiftBase gift)
+        {
+            gifts.Remove(gift);
+        }
+
+        public override int CalculateTotalPrice()
+        {
+            int totalPrice = 0;
+            Console.WriteLine($"{name} contains following products with prices:");
+
+            foreach (var gift in gifts)
+            {
+                totalPrice += gift.CalculateTotalPrice();
+            }
+
+            return totalPrice;
+        }
+
+    }
+}
---
+++ C# OOP/11.DesignPatternsExercise/CompositePattern/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CompositePattern
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var phone = new SingleGift("Phone", 256);
+            phone.CalculateTotalPrice();
+            Console.WriteLine();
+
+            var rootBox = new CompositeGift("RootBox", 0);
+            var truck = new SingleGift("Truck", 289);
+            var toy = new SingleGift("Toy", 587);
+            rootBox.Add(truck);
+            rootBox.Add(toy);
+
+            var childBox = new CompositeGift("ChildBox", 0);
+            var soldier = new SingleGift("Soldier", 200);
+            childBox.Add(soldier);
+
+            rootBox.Add(childBox);
+
+            Console.WriteLine($"Total price of this composite present is: {rootBox.CalculateTotalPrice()}");
+        }
+    }
+}
