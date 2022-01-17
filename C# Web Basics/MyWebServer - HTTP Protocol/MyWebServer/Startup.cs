344c4939966fd5830bf5ce4a75122d1eb733ca1d
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2022-01-17 14:25:40 +0200

    Http Protocol

---
+++ C# Web Basics/MyWebServer - HTTP Protocol/MyWebServer/Startup.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyWebServer.Server;
+using MyWebServer.Server.HTTP;
+using MyWebServer.Server.Responses;
+using System;
+
+namespace MyWebServer.Demo
+{
+    public class Startup
+    {
+        private const string HtmlForm = @"<form action='/HTML' method='POST'>
+                                        Name: <input type='text' name='Name'/>
+                                        Age: <input type='number' name ='Age'/>
+                                        <input type='submit' value ='Save' />
+                                        </form>";
+
+        static void Main(string[] args)
+        => new HttpServer(routes => routes
+        .MapGet("/", new TextResponse("Hello from the server!"))
+        .MapGet("/Redirect", new RedirectResponse("https://softuni.org/"))
+        .MapGet("/HTML", new HtmlResponse(Startup.HtmlForm))
+        .MapPost("/HTML", new TextResponse("", Startup.AddFormDataAction)))
+            .Start();
+
+        private static void AddFormDataAction(Request request, Response response)
+        {
+            response.Body = "";
+
+            foreach (var (key, value) in request.Form)
+            {
+                response.Body += $"{key} - {value}";
+                response.Body += Environment.NewLine;
+            }
+        }
+    }
+}
