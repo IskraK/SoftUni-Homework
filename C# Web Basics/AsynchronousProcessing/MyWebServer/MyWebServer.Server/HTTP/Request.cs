44f267e7fe7729321ef76ca30db6238e6d107b50
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2022-01-18 07:58:25 +0200

    Make Basic Web Server Asynchronous

---
+++ C# Web Basics/AsynchronousProcessing/MyWebServer/MyWebServer.Server/HTTP/Request.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MyWebServer.Server.HTTP
+{
+    public class Request
+    {
+        public Method Method { get; private set; }
+        public string Url { get; private set; }
+        public HeaderCollection Headers { get; private set; }
+        public string Body { get; private set; }
+
+        public IReadOnlyDictionary<string,string> Form { get; private set; }
+
+        public static Request Parse(string strRequest)
+        {
+            var lines = strRequest.Split("\r\n");
+            var firstLine = lines.First().Split(" ");
+            Method method = ParseMethod(firstLine[0]);
+            var url = firstLine[1];
+            HeaderCollection headers = ParseHeaders(lines.Skip(1));
+            var bodyLines = lines.Skip(headers.Count + 2).ToArray();
+            string body = string.Join("\r\n", bodyLines);
+            var form = ParseForm(headers, body);
+            
+            return new Request()
+            {
+                Method = method,
+                Url = url,
+                Headers = headers,
+                Body = body
+            };
+        }
+
+        private static Dictionary<string,string> ParseForm(HeaderCollection headers, string body)
+        {
+            var formCollection = new Dictionary<string, string>();
+
+            if (headers.Contains(Header.ContentType) && headers[Header.ContentType] == ContentType.FormUrlEncoded)
+            {
+                var parsedResult = ParseFormData(body);
+
+                foreach (var (name, value) in parsedResult)
+                {
+                    formCollection.Add(name, value);
+                }
+            }
+
+            return formCollection;
+        }
+
+        private static Dictionary<string, string> ParseFormData(string bodyLines)
+            => HttpUtility.UrlDecode(bodyLines)
+            .Split('&')
+            .Select(part => part.Split('='))
+            .Where(part => part.Length == 2)
+            .ToDictionary(part => part[0], part => part[1], StringComparer.InvariantCultureIgnoreCase);
+        
+
+        private static HeaderCollection ParseHeaders(IEnumerable<string> headerLines)
+        {
+            var headerCollection = new HeaderCollection();
+
+            foreach (var headerLine in headerLines)
+            {
+                if (headerLine == String.Empty)
+                {
+                    break;
+                }
+
+                var headerParts = headerLine.Split(":", 2);
+
+                if (headerParts.Length != 2)
+                {
+                    throw new InvalidOperationException("Request headers is not valid");
+                }
+
+                var headerName = headerParts[0];
+                var headerValue = headerParts[1].Trim();
+
+                headerCollection.Add(headerName, headerValue);
+            }
+
+            return headerCollection;
+        }
+
+        private static Method ParseMethod(string method)
+        {
+            try
+            {
+                return (Method)Enum.Parse(typeof(Method), method, true);
+                //return Enum.Parse<Method>(method);
+            }
+            catch (Exception)
+            {
+                throw new InvalidOperationException($"Method '{method}' is not supported");
+            }
+        }
+    }
+}
