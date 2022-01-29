3d07d408ecf5a3fa984c18a3ebf8b84d49a65a01
Iskra Krasimirova <iskra_krasimirova@abv.bg>
2022-01-29 21:11:22 +0200

    Add HomeController

--- C# Web Basics/MyWebServer/MyWebServer/Startup.cs
+++ C# Web Basics/MyWebServer/MyWebServer/Startup.cs
@@ -1,6 +1,8 @@
-﻿using MyWebServer.Server;
+﻿using MyWebServer.Demo.Comtrollers;
+using MyWebServer.Server;
 using MyWebServer.Server.HTTP;
 using MyWebServer.Server.Responses;
+using MyWebServer.Server.Routing;
 using System;
 using System.Text;
 using System.Web;
@@ -9,18 +11,6 @@ namespace MyWebServer.Demo
 {
     public class Startup
     {
-        private const string HtmlForm = @"<form action='/HTML' method='POST'>
-            Name: <input type='text' name='Name'/>
-            Age: <input type='number' name ='Age'/>
-            <input type='submit' value ='Save' />
-        </form>";
-
-        private const string DownloadForm = @"<form action='/Content' method='POST'>
-            <input type='submit' value ='Download Sites Content' /> 
-        </form>";
-
-
-        private const string FileName = "content.txt";
 
         private const string LoginForm = @"<form action='/Login' method='POST'>
                 Username: <input type='text' name='Username'/>
@@ -34,23 +24,20 @@ namespace MyWebServer.Demo
 
         static async Task Main(string[] args)
         {
-            //await DownloadSitesAsTextFile(Startup.FileName, new string[] { "https://judge.softuni.org/", "https://softuni.org/" });
-
-            var server = new HttpServer(routes => routes
-                .MapGet("/", new TextResponse("Hello from the server!"))
-                .MapGet("/Redirect", new RedirectResponse("https://softuni.org/"))
-                .MapGet("/HTML", new HtmlResponse(Startup.HtmlForm))
-                .MapPost("/HTML", new TextResponse("", Startup.AddFormDataAction))
-                .MapGet("/Content", new HtmlResponse(Startup.DownloadForm))
-                .MapPost("/Content", new FileResponse(Startup.FileName))
-                .MapGet("/Cookies", new HtmlResponse("", Startup.AddCookiesAction))
-                .MapGet("/Session", new TextResponse("",Startup.DisplaySessionInfoAction))
-                .MapGet("/Login", new HtmlResponse(Startup.LoginForm))
-                .MapPost("/Login", new HtmlResponse("", Startup.LoginAction))
-                .MapGet("/Logout", new HtmlResponse("", Startup.LogoutAction))
-                .MapGet("/UserProfile", new HtmlResponse("",Startup.GetUserDataAction)));
-
-            await server.Start();
+            await new HttpServer(routes => routes
+                .MapGet<HomeController>("/", c => c.Index())
+                .MapGet<HomeController>("/Redirect", c => c.Redirect())
+                .MapGet<HomeController>("/HTML", c => c.Html())
+                .MapPost<HomeController>("/HTML", c => c.HtmlFormPost())
+                .MapGet<HomeController>("/Content", c => c.Content())
+                .MapPost<HomeController>("/Content", c => c.DownloadContent())
+                .MapGet<HomeController>("/Cookies", c => c.Cookies())
+                .MapGet<HomeController>("/Session", c => c.Session()))
+                //.MapGet<HomeController>("/Login", new HtmlResponse(Startup.LoginForm))
+                //.MapPost<HomeController>("/Login", new HtmlResponse("", Startup.LoginAction))
+                //.MapGet<HomeController>("/Logout", new HtmlResponse("", Startup.LogoutAction))
+                //.MapGet<HomeController>("/UserProfile", new HtmlResponse("",Startup.GetUserDataAction)))
+                .Start();
         }
 
         private static void GetUserDataAction(Request request, Response response)
@@ -100,94 +87,5 @@ namespace MyWebServer.Demo
             response.Body += bodyText;
         }
 
-        private static void DisplaySessionInfoAction(Request request, Response response)
-        {
-            var sessionExists=request.Session.ContainsKey(Session.SessionCurrentDateKey);
-            var bodyText = "";
-
-            if (sessionExists)
-            {
-                var currentDate = request.Session[Session.SessionCurrentDateKey];
-
-                bodyText = $"Stored date: {currentDate}";
-            }
-            else
-            {
-                bodyText = $"Current date stored!";
-            }
-
-            response.Body = "";
-            response.Body += bodyText;
-        }
-
-        private static void AddCookiesAction(Request request, Response response)
-        {
-            var requestHasCookise = request.Cookies.Any(c => c.Name != Session.SessionCookieName);
-            var bodyText = "";
-
-            if (requestHasCookise)
-            {
-                StringBuilder cookieText = new StringBuilder();
-                cookieText.AppendLine("<h1>Cookies</h1>");
-
-                cookieText.Append("<table border='1'><tr><th>Name</th><th>Value</th></tr>");
-
-                foreach (var cookie in request.Cookies)
-                {
-                    cookieText.Append("<tr>");
-                    cookieText.Append($"<td>{HttpUtility.HtmlEncode(cookie.Name)}</td>");
-                    cookieText.Append($"<td>{HttpUtility.HtmlEncode(cookie.Value)}</td>");
-                    cookieText.Append("</tr>");
-                }
-
-                cookieText.Append("</table>");
-                bodyText = cookieText.ToString();
-            }
-            else
-            {
-                bodyText = "<h1>Cookies set!</h1>";
-                response.Cookies.Add("My-Cookie", "My-Value");
-                response.Cookies.Add("My-Second-Cookie", "My-Second-Value");
-            }
-
-            response.Body += bodyText;   
-        }
-
-        private static void AddFormDataAction(Request request, Response response)
-        {
-            response.Body = "";
-
-            foreach (var (key, value) in request.Form)
-            {
-                response.Body += $"{key} - {value}";
-                response.Body += Environment.NewLine;
-            }
-        }
-
-        private static async Task<string> DownloadWebSiteContent(string url)
-        {
-            var httpClient = new HttpClient();
-            using (httpClient)
-            {
-                var response = await httpClient.GetAsync(url);
-                var html = await response.Content.ReadAsStringAsync();
-
-                return html.Substring(0, 2000);
-            }
-        }
-
-        private static async Task DownloadSitesAsTextFile(string fileName, string[] urls)
-        {
-            var downloads = new List<Task<string>>();
-
-            foreach (var url in urls)
-            {
-                downloads.Add(DownloadWebSiteContent(url));
-            }
-
-            var responses = await Task.WhenAll(downloads);
-            var responsesString = string.Join(Environment.NewLine + new String('-', 100), responses);
-            await File.WriteAllTextAsync(fileName, responsesString);
-        }
     }
 }
